ffe20afcffc64334b42a82b7c6d3235842384f88
Lauren <Laurenelee88@gmail.com>
2017-10-16 15:38:56 -0700

    functioning edit and delete abilities

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :require_login
+  before_action :find_user, :require_login
 
   def render_404
     # DPR: supposedly this will actually render a 404 page in production
@@ -36,9 +36,9 @@ class ApplicationController < ActionController::Base
 
 
   private
-  # def find_user
-  #   if session[:user_id]
-  #     @login_user = User.find_by(id: session[:user_id])
-  #   end
-  # end
+  def find_user
+    if session[:user_id]
+      @login_user = User.find_by(id: session[:user_id])
+    end
+  end
 end
