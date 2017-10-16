e1fed7a5872f646ec001600ae69498246d724880
Lauren <Laurenelee88@gmail.com>
2017-10-16 12:25:14 -0700

    oauth work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,41 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :find_user
+  before_action :find_user, :require_login
 
   def render_404
     # DPR: supposedly this will actually render a 404 page in production
     raise ActionController::RoutingError.new('Not Found')
   end
 
-private
+
+  protected
+  def save_and_flash(model)
+    result = model.save
+
+    if result
+      flash[:status] = :success
+      flash[:message] = "Successfully saved #{model.class} #{model.id}"
+    else
+      flash.now[:status] = :failure
+      flash.now[:message] = "Failed to save #{model.class}"
+      flash.now[:details] = model.errors.messages
+    end
+
+    return result
+  end
+
+  def require_login
+    @user = User.find_by(id: session[:user_id])
+    unless @user
+      flash[:status] = :failure
+      flash[:message] = "You must be logged in to do that!"
+      redirect_to root_path
+    end
+  end
+
+  
+  private
   def find_user
     if session[:user_id]
       @login_user = User.find_by(id: session[:user_id])
